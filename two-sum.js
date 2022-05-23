bb39f1587b8716a96520f472034f4324fece6b96
Mark Charles Smith <markcharlessmith@gmail.com>
2022-05-23 14:45:32 -0700

    updated two-sum.js

--- two-sum.js
+++ two-sum.js
@@ -6,4 +6,18 @@ You may assume that each input would have exactly one solution, and you may not 
 
 You can return the answer in any order.*/
 
-// function twoSum ()
+// input: array of numbers, target integer
+// output: array of indices
+
+// O(n2) time complexity
+function twoSum(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) return [i, j];
+    }
+  }
+  return 'No two numbers sum to the target.';
+}
+
+console.log(twoSum([1, 3, 5, 7], 10)); // => should return 1, 3 (indexes of the numbers that add up to the target)
+console.log(twoSum([1, 2, 3, 7], 11));
