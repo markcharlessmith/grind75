ba9c54b715967ec3e33447f6d18f2f02e441a9c0
Mark Charles Smith <markcharlessmith@gmail.com>
2022-05-26 14:59:49 -0700

    updated valid-palindrome.js

--- valid-palindrome.js
+++ valid-palindrome.js
@@ -2,28 +2,36 @@
 
 Given a string s, return true if it is a palindrome, or false otherwise.
 
- 
-
 Example 1:
-
 Input: s = "A man, a plan, a canal: Panama"
 Output: true
 Explanation: "amanaplanacanalpanama" is a palindrome.
-Example 2:
 
+Example 2:
 Input: s = "race a car"
 Output: false
 Explanation: "raceacar" is not a palindrome.
-Example 3:
 
+Example 3:
 Input: s = " "
 Output: true
 Explanation: s is an empty string "" after removing non-alphanumeric characters.
 Since an empty string reads the same forward and backward, it is a palindrome.
  
-
 Constraints:
 
 1 <= s.length <= 2 * 105
 s consists only of printable ASCII characters.
 */
+
+function validPalindrome(string) {
+  // use regex
+  // use .toLowerCase()
+  // recursively slice through the string
+}
+
+const string1 = 'A man, a plan, a canal: Panama';
+console.log(validPalindrome(string1)); //should return true;
+
+const string2 = 'race a car';
+console.log(validPalindrome(string2)); //should return false;
