2bf0c869cee6659e44c785c88515600015a88d6c
Mark Charles Smith <markcharlessmith@gmail.com>
2022-05-27 11:14:18 -0700

    reworked two-sum.js O(n) solution

--- two-sum.js
+++ two-sum.js
@@ -19,20 +19,35 @@ You can return the answer in any order.*/
 //   return 'No two numbers sum to the target.';
 // }
 
-// O(n) time complexity solution using a hashmap
+// O(n) solution
 function twoSum(arr, target) {
-  let cache = {};
+  // declare an empty object
+  const cache = {};
+  // iterate through the input array
   for (let i = 0; i < arr.length; i++) {
+    // if the difference between the target and array at index i is in the cache,
     if (target - arr[i] in cache) {
+      // console.log(target, arr[i], target - arr[i]);
+      // console.log(cache);
+      // return an array of the cache at the index of the target minus the array at index i, and i
       return [cache[target - arr[i]], i];
+      // else, assign cache at array at i to equal i
     } else {
       cache[arr[i]] = i;
-      console.log(cache);
+      // console.log(cache[arr[i]]);
     }
   }
   return 'No two numbers sum to the target.';
 }
 
-console.log(twoSum([1, 3, 5, 7], 10)); // => should return 1, 3 (indexes of the numbers that add up to the target)
-console.log(twoSum([1, 2, 5, 7], 10)); // => should return 'No two numbers sum to the target.' - demonstrating that the same element is not being used twice (5+5)
-console.log(twoSum([1, 2, 3, 7], 11)); // => should return 'No two numbers sum to the target.'
+console.log(twoSum([1, 3, 5, 7], 10));
+// => should return 1, 3 (indexes of the numbers that add up to the target)
+
+console.log(twoSum([-1, -4, -5, 3, -7, 2, 14], -8));
+// => should return 0, 4
+
+// console.log(twoSum([1, 2, 5, 7], 10));
+// => should return 'No two numbers sum to the target.' - demonstrating that the same element is not being used twice (5+5)
+
+// console.log(twoSum([1, 2, 3, 7], 11));
+// => should return 'No two numbers sum to the target.'
