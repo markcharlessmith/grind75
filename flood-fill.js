0bea395ba1517d41d8ac4a6071952226085859a9
Mark Charles Smith <markcharlessmith@gmail.com>
2022-06-05 09:20:23 -0700

    added notes and test case for flood-fill.js

--- flood-fill.js
+++ flood-fill.js
@@ -9,14 +9,12 @@ Return the modified image after performing the flood fill.
  
 
 Example 1:
-
-
 Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, newColor = 2
 Output: [[2,2,2],[2,2,0],[2,0,1]]
 Explanation: From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
 Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
-Example 2:
 
+Example 2:
 Input: image = [[0,0,0],[0,0,0]], sr = 0, sc = 0, newColor = 2
 Output: [[2,2,2],[2,2,2]]
  
@@ -31,4 +29,19 @@ n == image[i].length
 0 <= sc < n
 */
 
-function floodFill() {}
+// notes: sr represents starting row, sc represents starting column
+
+function floodFill(image, sr, sc, newColor) {}
+
+console.log(
+  floodFill(
+    [
+      [1, 1, 1],
+      [1, 1, 0],
+      [1, 0, 1],
+    ],
+    1,
+    1,
+    2
+  )
+);
