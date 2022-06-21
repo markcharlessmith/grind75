06b4a5397a13dd3c2dd02b032f6a8d03e75ca9c0
Mark Charles Smith <markcharlessmith@gmail.com>
2022-06-21 13:05:09 -0700

    updated ransom-note.js

--- ransom-note.js
+++ ransom-note.js
@@ -19,17 +19,40 @@ ransomNote and magazine consist of lowercase English letters.*/
 function scaryArtProject(ransomNote, magazine) {
   // declare an object
   const letterBank = {};
+  const assembledNote = {};
 
   // split the magazine string (and sorting for alphabetic order)
   const cutLetters = magazine.split('').sort();
-  console.log(cutLetters);
+  // console.log(cutLetters);
   // iterate through each character in cutLetters array and store instances of letter in object
   for (let i = 0; i < cutLetters.length; i++) {
     if (!letterBank[cutLetters[i]]) letterBank[cutLetters[i]] = 1;
     else letterBank[cutLetters[i]] += 1;
   }
   console.log(letterBank);
-  // iterate through ransomNote and lookup in object to see if required letters present
+
+  // iterate through split ransomNote and lookup in object to see if required letters present
+  const neededLetters = ransomNote.split('').sort();
+  // console.log(neededLetters);
+  for (let i = 0; i < neededLetters.length; i++) {
+    if (!assembledNote[neededLetters[i]]) assembledNote[neededLetters[i]] = 1;
+    else assembledNote[neededLetters[i]] += 1;
+  }
+  console.log(assembledNote);
+
+  // does each key of assembledNote exist as a key on letterBank
+  for (const key in Object.keys(letterBank)) {
+    console.log(key);
+  }
+
+  // iterate through the keys of assembledNote and compare to letterBank
+  // for (const [key, value] in Object.entries(assembledNote)) {
+  // for (const key in assembledNote) {
+  // console.log(letterBank.key);
+  // if (!letterBank.key >= assembledNote.key) return false;
+  // if (letterBank.key)
+  // }
+  // else return true;
 }
 
 // console.log(scaryArtProject('a', 'b')); // expect false
