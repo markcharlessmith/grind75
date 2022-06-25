85b54bc8f1c44d7c5aeaadf7598f603f277290a3
Mark Charles Smith <markcharlessmith@gmail.com>
2022-06-25 10:16:06 -0700

    completed solution for min-stack.js

--- min-stack.js
+++ min-stack.js
@@ -29,17 +29,56 @@ minStack.top();    // return 0
 minStack.getMin(); // return -2
 */
 
+function last(stack) {
+  return stack[stack.length - 1];
+}
+
 // initializes the stack object
-function MinStack() {}
+function MinStack() {
+  this.stack = [];
+  this.minStack = [];
+}
 
 // pushes the element val onto the stack
-MinStack.prototype.push = function (val) {};
+MinStack.prototype.push = function (val) {
+  if (this.minStack.length === 0 || val <= last(this.minStack)) {
+    this.minStack.push(val);
+  }
+  this.stack.push(val);
+};
 
 // removes the element on the top of the stack
-MinStack.prototype.pop = function () {};
+MinStack.prototype.pop = function () {
+  if (last(this.minStack) === last(this.stack)) {
+    this.minStack.pop();
+  }
+  return this.stack.pop();
+};
 
 // returns the top element of the stack
-MinStack.prototype.top = function () {};
+MinStack.prototype.top = function () {
+  console.log(last(this.stack));
+  return last(this.stack);
+};
 
 // retrieves the minimum element in the stack
-MinStack.prototype.getMin = function () {};
+MinStack.prototype.getMin = function () {
+  console.log(last(this.minStack));
+  return last(this.minStack);
+};
+
+const minStack = new MinStack();
+minStack.push(-2);
+console.log(minStack);
+minStack.push(0);
+console.log(minStack);
+minStack.push(-3);
+console.log(minStack);
+minStack.getMin(); // return -3
+console.log(minStack);
+minStack.pop();
+console.log(minStack);
+minStack.top();
+console.log(minStack); // return 0
+minStack.getMin(); // return -2
+console.log(minStack);
