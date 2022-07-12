399453bb7b881b37880847043634d1de50afc8cf
Mark Charles Smith <markcharlessmith@gmail.com>
2022-07-12 16:56:53 -0700

    added pseudocode and test cases to 01-matrix.js

--- 01-matrix.js
+++ 01-matrix.js
@@ -22,4 +22,20 @@ There is at least one 0 in mat.
  * @param {number[][]} mat
  * @return {number[][]}
  */
-var updateMatrix = function (mat) {};
+var updateMatrix = function (mat) {
+  // look left, look up, look right, look down
+};
+
+const matrix1 = [
+  [0, 0, 0],
+  [0, 1, 0],
+  [0, 0, 0],
+];
+console.log(updateMatrix(matrix1)); // expect [[0,0,0],[0,1,0],[0,0,0]]
+
+const matrix2 = [
+  [0, 0, 0],
+  [0, 1, 0],
+  [1, 1, 1],
+];
+console.log(updateMatrix(matrix2)); // expect [[0,0,0],[0,1,0],[1,2,1]]
