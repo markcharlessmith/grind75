3c73b4c5b4aa24be490aee9f9d94db2515cb9af3
Mark Charles Smith <markcharlessmith@gmail.com>
2022-07-20 12:35:26 -0700

    added solution for 01-matrix

--- 01-matrix.js
+++ 01-matrix.js
@@ -22,39 +22,70 @@ There is at least one 0 in mat.
  * @param {number[][]} mat
  * @return {number[][]}
  */
-var updateMatrix = function (mat) {
-  // create a resultMatrix
-  const resultMatrix = [];
-  // console.log(mat.length);
-  let rowCount = mat.length;
-  while (rowCount > 0) {
-    resultMatrix.push([]);
-    rowCount--;
-  }
-  let columnCount = mat[0].length;
-  console.log(mat[0].length);
-  for (let i = 0; i < mat[0].length; i++) {
-    // console.log(resultMatrix[i]);
-    while (resultMatrix[i].length < mat[0].length) {
-      resultMatrix[i].push(0);
-    }
-  }
-  console.log(resultMatrix);
-  // successfully created a result matrix which meets the criteria specified
+// var updateMatrix = function (mat) {
+//   // create a resultMatrix
+//   const resultMatrix = [];
+//   // console.log(mat.length);
+//   let rowCount = mat.length;
+//   while (rowCount > 0) {
+//     resultMatrix.push([]);
+//     rowCount--;
+//   }
+//   let columnCount = mat[0].length;
+//   console.log(mat[0].length);
+//   for (let i = 0; i < mat[0].length; i++) {
+//     // console.log(resultMatrix[i]);
+//     while (resultMatrix[i].length < mat[0].length) {
+//       resultMatrix[i].push(0);
+//     }
+//   }
+//   console.log(resultMatrix);
+//   // successfully created a result matrix which meets the criteria specified
 
-  // iterate through the input matrix
-  for (let i = 0; i < mat.length; i++) {
-    for (let j = 0; j < mat[i].length; j++) {
-      if (mat[i][j] !== 0) {
-        resultMatrix[i][j] = mat[i][j];
-        if (mat[i - 1][j] === 1) resultMatrix[i][j] += 1;
-        // if (mat[i+1] && mat[i + 1][j] === 1) resultMatrix[i][j] += 1;
+//   // iterate through the input matrix
+//   for (let i = 0; i < mat.length; i++) {
+//     for (let j = 0; j < mat[i].length; j++) {
+//       if (mat[i][j] !== 0) {
+//         resultMatrix[i][j] = mat[i][j];
+//         // enclose the below in a while loop
+//         if (mat[i - 1][j] !== 0) resultMatrix[i][j] += 1;
+//         // if (mat[i+1] && mat[i + 1][j] === 1) resultMatrix[i][j] += 1;
+//       }
+//     }
+//   }
+
+//   // look left, look up, look right, look down
+//   return resultMatrix;
+// };
+
+var updateMatrix = function (mat) {
+  var knownZeros = [];
+
+  for (var i = 0; i < mat.length; i++) {
+    for (var j = 0; j < mat[i].length; j++) {
+      if (mat[i][j] === 0) {
+        knownZeros.push([i, j]);
       }
     }
   }
 
-  // look left, look up, look right, look down
-  return resultMatrix;
+  for (var i = 0; i < mat.length; i++) {
+    for (var j = 0; j < mat[i].length; j++) {
+      if (mat[i][j] === 1) {
+        var smallestDelta = null;
+
+        for (var k = 0; k < knownZeros.length; k++) {
+          var delta =
+            Math.abs(i - knownZeros[k][0]) + Math.abs(j - knownZeros[k][1]);
+          if (smallestDelta === null || delta < smallestDelta) {
+            smallestDelta = delta;
+          }
+        }
+        mat[i][j] = smallestDelta;
+      }
+    }
+  }
+  return mat;
 };
 
 const matrix1 = [
@@ -70,3 +101,12 @@ const matrix2 = [
   [1, 1, 1],
 ];
 console.log(updateMatrix(matrix2)); // expect [[0,0,0],[0,1,0],[1,2,1]]
+
+const matrix3 = [
+  [0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 0],
+  [0, 1, 1, 1, 0],
+  [0, 1, 1, 1, 0],
+  [0, 1, 1, 1, 0],
+];
+console.log(updateMatrix(matrix3));
