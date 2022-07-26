1ba9b17a0b84590fb060dcb4635760b216137c70
Mark Charles Smith <markcharlessmith@gmail.com>
2022-07-25 18:06:03 -0700

    solved longest-substring-wo-repeating.js

--- longest-substring-wo-repeating.js
+++ longest-substring-wo-repeating.js
@@ -21,7 +21,40 @@ Notice that the answer must be a substring, "pwke" is a subsequence and not a su
  * @param {string} s
  * @return {number}
  */
-const lengthOfLongestSubstring = function (s) {};
+// const lengthOfLongestSubstring = function (s) {
+//   let resultString = '';
+//   let count = 0;
+//   for (let i = count; i < s.length; i++) {
+//     console.log(resultString);
+//     if (resultString.includes(s[i])) {
+//     }
+//     if (!resultString.includes(s[i])) {
+//       resultString += s[i];
+//       count += 1;
+//       // console.log(resultString);
+//     }
+//   }
+//   return count;
+// };
+
+const lengthOfLongestSubstring = function (s) {
+  const substring = new Set();
+  let longest = 0;
+  let first = 0,
+    last = 0;
+
+  while (last < s.length) {
+    if (substring.has(s[last])) {
+      substring.delete(s[first]);
+      first++;
+    } else {
+      substring.add(s[last]);
+      longest = Math.max(longest, substring.size);
+      last++;
+    }
+  }
+  return longest;
+};
 
 console.log(lengthOfLongestSubstring('abcabcbb')); // expect 3
 console.log(lengthOfLongestSubstring('bbbbb')); // expect 1
