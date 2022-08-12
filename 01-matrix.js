6ce1b6fb078b5888ca426d49373b6d591f7bfeee
Mark Charles Smith <markcharlessmith@gmail.com>
2022-08-12 13:38:59 -0700

    cleaned up solution for 01-matrix.js

--- 01-matrix.js
+++ 01-matrix.js
@@ -18,46 +18,6 @@ mat[i][j] is either 0 or 1.
 There is at least one 0 in mat.
 */
 
-/**
- * @param {number[][]} mat
- * @return {number[][]}
- */
-// var updateMatrix = function (mat) {
-//   // create a resultMatrix
-//   const resultMatrix = [];
-//   // console.log(mat.length);
-//   let rowCount = mat.length;
-//   while (rowCount > 0) {
-//     resultMatrix.push([]);
-//     rowCount--;
-//   }
-//   let columnCount = mat[0].length;
-//   console.log(mat[0].length);
-//   for (let i = 0; i < mat[0].length; i++) {
-//     // console.log(resultMatrix[i]);
-//     while (resultMatrix[i].length < mat[0].length) {
-//       resultMatrix[i].push(0);
-//     }
-//   }
-//   console.log(resultMatrix);
-//   // successfully created a result matrix which meets the criteria specified
-
-//   // iterate through the input matrix
-//   for (let i = 0; i < mat.length; i++) {
-//     for (let j = 0; j < mat[i].length; j++) {
-//       if (mat[i][j] !== 0) {
-//         resultMatrix[i][j] = mat[i][j];
-//         // enclose the below in a while loop
-//         if (mat[i - 1][j] !== 0) resultMatrix[i][j] += 1;
-//         // if (mat[i+1] && mat[i + 1][j] === 1) resultMatrix[i][j] += 1;
-//       }
-//     }
-//   }
-
-//   // look left, look up, look right, look down
-//   return resultMatrix;
-// };
-
 var updateMatrix = function (mat) {
   var knownZeros = [];
 
