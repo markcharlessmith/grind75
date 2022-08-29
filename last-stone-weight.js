3d8ef1ff69f6f0b2c23606ba5d1ceb99c180e5a1
Mark Charles Smith <markcharlessmith@gmail.com>
2022-08-29 16:40:26 -0700

    solved last-stone-weight.js

--- last-stone-weight.js
+++ last-stone-weight.js
@@ -1,5 +1,4 @@
 /*You are given an array of integers stones where stones[i] is the weight of the ith stone.
-
 We are playing a game with the stones. On each turn, we choose the heaviest two stones and smash them together. Suppose the heaviest two stones have weights x and y with x <= y. The result of this smash is:
 
 If x == y, both stones are destroyed, and
@@ -8,19 +7,16 @@ At the end of the game, there is at most one stone left.
 
 Return the weight of the last remaining stone. If there are no stones left, return 0.
 
- 
-
 Example 1:
-
 Input: stones = [2,7,4,1,8,1]
 Output: 1
 Explanation: 
 We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
 we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
 we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
-we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of the last stone.
-Example 2:
+we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of the last stone.\
 
+Example 2:
 Input: stones = [1]
 Output: 1
 */
@@ -29,4 +25,33 @@ Output: 1
  * @param {number[]} stones
  * @return {number}
  */
-var lastStoneWeight = function (stones) {};
+const lastStoneWeight = function (stones) {
+  let greatest = -Infinity;
+  let nextGreatest = -Infinity;
+  let difference = 0;
+  // sort the stones array
+  stones = stones.sort((a, b) => {
+    return b - a;
+  });
+  console.log(stones);
+  // for (let i = 0; i < stones.length; i++) {
+  while (stones.length > 1) {
+    // take the difference of the two greatest values
+    greatest = stones[0];
+    nextGreatest = stones[1];
+    console.log([greatest, nextGreatest]);
+    difference = greatest - nextGreatest;
+    console.log(difference);
+    stones.push(difference);
+    stones.shift();
+    stones.shift();
+    console.log(stones);
+  }
+  return stones[0];
+};
+
+const pileOfStones1 = [2, 7, 4, 1, 8, 1];
+console.log(lastStoneWeight(pileOfStones1)); // expect 1
+
+// const pileOfStones2 = [1];
+// console.log(lastStoneWeight(pileOfStones2)); // expect 1
