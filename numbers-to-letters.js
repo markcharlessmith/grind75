38375cd482d1165e8b7dfc9b5fe1f747cb729bd3
Mark Charles Smith <markcharlessmith@gmail.com>
2022-09-16 13:29:57 -0700

    completed solution for numbers-to-letters.js

--- numbers-to-letters.js
+++ numbers-to-letters.js
@@ -1,36 +1,56 @@
-/* Given a string of letters and umbers, output the letter following a number as many times as the number before it
+/* Given a string of letters and numbers, output the letter following a number as many times as the number before it
 For a simpler input, '2c1b3t' should return 'ccbttt'
 For a more complex input, '12g4s1p' should return 'ggggggggggggssssp'
 */
 function letterfier(str) {
-  // initialize a result string
-  let result = '';
+  // initialize a result array
+  let result = [];
   // split the string
   str = str.split('');
-  // iterate through the string array, if the character at index i is a number, reassign it to be a number
+  // iterate through the string array
   for (let i = 0; i < str.length; i++) {
+    // if str[i] and str[i-1] are numbers, concat them, and then splice i
     if (Number(str[i]) && Number(str[i - 1])) {
       str[i - 1] = str[i - 1].concat(str[i]);
       str.splice(i, 1);
-      console.log(str[i]);
     }
-    // if (Number(str[i])) {
-    // console.log(str[i]);
-    // str[i] = Number(str[i]);
-    // }
   }
-  // iterate through the string array, if there are two consecutive numbers, join them together
-
   console.log(str);
+  // initialize a tempArray
+  const tempArray = [];
+  // iterate through the string array
+  for (let i = 0; i < str.length; i++) {
+    // if str[i] is a number, push the number to the tempArray
+    if (Number(str[i])) {
+      tempArray.push(Number(str[i]));
+      // else push the character
+    } else {
+      tempArray.push(str[i]);
+    }
+  }
+  console.log(tempArray);
 
-  // check if the previous
-
+  // iterate through tempArray
+  // if tempArray[i] is a string && tempArray[i - 1] is a number,
+  // push tempArray[i] to result that number of times
+  for (let i = 0; i < tempArray.length; i++) {
+    if (
+      typeof tempArray[i] === 'string' &&
+      typeof tempArray[i - 1] === 'number'
+    ) {
+      while (tempArray[i - 1] >= 1) {
+        result.push(tempArray[i]);
+        tempArray[i - 1]--;
+      }
+    }
+  }
   // return result;
-  return result;
+  return result.join('');
 }
+// }
 
 let str1 = '2c1b3t';
-// console.log(letterfier(str1)); // expect 'ccbttt'
+console.log(letterfier(str1)); // expect 'ccbttt'
 
 let str2 = '12g4s1p';
 console.log(letterfier(str2)); // expect 'ggggggggggggssssp'
