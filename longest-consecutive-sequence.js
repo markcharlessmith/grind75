a0740b87f5562b5e74f796a5010eae66ce54d18a
Mark Charles Smith <markcharlessmith@gmail.com>
2022-09-21 13:06:17 -0700

    added optimized solution for longest-consecutive-sequence.js using Set

--- longest-consecutive-sequence.js
+++ longest-consecutive-sequence.js
@@ -19,33 +19,53 @@ Constraints:
  * @param {number[]} nums
  * @return {number}
  */
-var longestConsecutive = function (nums) {
-  // edge case: if array is empty, return 0
-  if (nums.length === 0) return 0;
-  // sort the input array, and remove duplicates using filter
-  nums = nums
-    .sort((a, b) => a - b)
-    .filter((el, index) => nums.indexOf(el) === index);
-  console.log(nums);
+// var longestConsecutive = function (nums) {
+//   // edge case: if array is empty, return 0
+//   if (nums.length === 0) return 0;
+//   // sort the input array, and remove duplicates using filter
+//   nums = nums
+//     .sort((a, b) => a - b)
+//     .filter((el, index) => nums.indexOf(el) === index);
+//   console.log(nums);
 
-  let counter = 0;
-  let maxConsecCount = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === nums[i + 1] - 1) {
-      console.log([nums[i], nums[i + 1] - 1]);
-      counter += 1;
-      if (counter >= maxConsecCount) maxConsecCount = counter;
-    } else {
-      counter = 0;
+//   let counter = 0;
+//   let maxConsecCount = 0;
+//   for (let i = 0; i < nums.length; i++) {
+//     if (nums[i] === nums[i + 1] - 1) {
+//       console.log([nums[i], nums[i + 1] - 1]);
+//       counter += 1;
+//       if (counter >= maxConsecCount) maxConsecCount = counter;
+//     } else {
+//       counter = 0;
+//     }
+//     console.log(counter);
+//   }
+//   console.log(maxConsecCount);
+//   // return result.length;
+//   return 1 + maxConsecCount;
+// };
+
+// optimized solution using set
+var longestConsecutive = function (nums) {
+  const set = new Set(nums);
+  let maxScore = 0;
+
+  set.forEach((el) => {
+    if (set.has(el - 1)) return;
+
+    let score = 1,
+      curr = el;
+    while (set.has(curr + 1)) {
+      curr++;
+      score++;
     }
-    console.log(counter);
-  }
-  console.log(maxConsecCount);
-  // return result.length;
-  return 1 + maxConsecCount;
+
+    maxScore = Math.max(maxScore, score);
+  });
+  return maxScore;
 };
 
 console.log(longestConsecutive([100, 4, 200, 1, 3, 2])); // expect 4
 console.log(longestConsecutive([0, 3, 7, 2, 5, 8, 4, 6, 0, 1])); // expect 9
 console.log(longestConsecutive([0])); // expect 1
-console.log(longestConsecutive([]));
+console.log(longestConsecutive([])); // expect 0
