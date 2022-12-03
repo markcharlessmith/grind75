e7dfd240be7f5771a12c58973db0b96733c7906e
Mark Charles Smith <markcharlessmith@gmail.com>
2022-12-02 16:07:25 -0800

    optimal solution added for greatest-element-on-right-side.js

--- greatest-element-on-right-side.js
+++ greatest-element-on-right-side.js
@@ -21,29 +21,44 @@ Output: [-1]
 Explanation: There are no elements to the right of index 0.
 */
 
+// function replaceElements(arr) {
+//   if (arr.length <= 1) return [-1];
+//   if (!Array.isArray(arr)) return [-1];
+//   const result = Array.from(arr);
+//   for (let i = 0; i < result.length; i++) {
+//     result[i] = Math.max(...result.slice(i + 1));
+//     console.log(result[i]);
+//   }
+//   result[result.length - 1] = -1;
+//   return result;
+// }
+
+// optimized solution
 function replaceElements(arr) {
-  if (arr.length <= 1) return [-1];
-  if (!Array.isArray(arr)) return [-1];
-  const result = Array.from(arr);
-  for (let i = 0; i < result.length; i++) {
-    result[i] = Math.max(...result.slice(i + 1));
-    console.log(result[i]);
+  if (arr.length <= 1 || !Array.isArray(arr)) return [-1];
+
+  let max = -1;
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    const curr = arr[i];
+    arr[i] = max;
+    max = Math.max(max, curr);
   }
-  result[result.length - 1] = -1;
-  return result;
+
+  return arr;
 }
 
 const arr1 = [17, 18, 5, 4, 6, 1];
 console.log(replaceElements(arr1)); // expect [18,6,6,6,1,-1]
 
-// const arr2 = [400];
-// console.log(replaceElements(arr2)); // expect [-1]
+const arr2 = [400];
+console.log(replaceElements(arr2)); // expect [-1]
 
-// const arr3 = [];
-// console.log(replaceElements(arr3)); // expect [-1]
+const arr3 = [];
+console.log(replaceElements(arr3)); // expect [-1]
 
-// const arr4 = [-5, -1, 1, 14, 12, 2, 0];
-// console.log(replaceElements(arr4)); // expect [14,14,14,12,2,0,-1]
+const arr4 = [-5, -1, 1, 14, 12, 2, 0];
+console.log(replaceElements(arr4)); // expect [14,14,14,12,2,0,-1]
 
-// const arr5 = "I'm totally not an array";
-// console.log(replaceElements(arr5));
+const arr5 = "I'm totally not an array";
+console.log(replaceElements(arr5));
