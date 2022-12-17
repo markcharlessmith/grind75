aa1081f8e05769acf5a5843cd8615a03f6f773cd
Mark Charles Smith <markcharlessmith@gmail.com>
2022-12-16 17:37:42 -0800

    additional way of solving longest-common-subsequence.js using 2D DP

--- longest-common-subsequence.js
+++ longest-common-subsequence.js
@@ -62,34 +62,87 @@ Explanation: There is no such common subsequence, so the result is 0.*/
 // };
 
 // dynamic programming solution
-var longestCommonSubsequence = function (text1, text2) {
-  const dp = [];
-  let m = text1.length;
-  let n = text2.length;
+// var longestCommonSubsequence = function (text1, text2) {
+//   const dp = [];
+//   let m = text1.length;
+//   let n = text2.length;
 
-  for (let i = 0; i <= m; i++) {
-    dp[i] = new Array(n + 1).fill(0);
-  }
+//   for (let i = 0; i <= m; i++) {
+//     dp[i] = new Array(n + 1).fill(0);
+//   }
 
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      // two  possible scenarioes:
-      // 1. the current char of text1 and text2 does not match
-      // 2. the current char of text1 and text2 matches
+//   for (let i = 1; i <= m; i++) {
+//     for (let j = 1; j <= n; j++) {
+//       // two  possible scenarioes:
+//       // 1. the current char of text1 and text2 does not match
+//       // 2. the current char of text1 and text2 matches
 
-      if (text1.charAt(i - 1) != text2.charAt(j - 1)) {
-        // check left and top for longer subsequence length
-        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-      } else {
-        // check diag for prev longest subsequence length and add 1
-        dp[i][j] = dp[i - 1][j - 1] + 1;
-      }
+//       if (text1.charAt(i - 1) != text2.charAt(j - 1)) {
+//         // check left and top for longer subsequence length
+//         dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+//       } else {
+//         // check diag for prev longest subsequence length and add 1
+//         dp[i][j] = dp[i - 1][j - 1] + 1;
+//       }
+//     }
+//   }
+
+//   console.log(dp);
+//   return dp[m][n];
+// };
+
+// var longestCommonSubsequence = function (text1, text2) {
+//   let dp = new Array(text1.length + 1)
+//     .fill(null)
+//     .map(() => new Array(text2.length + 1).fill(0));
+
+//   for (let i = 1; i <= text1.length; i++) {
+//     for (let j = 1; j <= text2.length; j++) {
+//       if (text1[i - 1] === text2[j - 1]) {
+//         dp[i][j] = dp[i - 1][j - 1] + 1;
+//       } else {
+//         dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+//       }
+//     }
+//   }
+
+//   console.log(dp);
+
+//   return dp[text1.length][text2.length];
+// };
+
+// DP solution from Neetcode
+var longestCommonSubsequence = (text1, text2) => {
+  const tabu = initTabu(text1, text2); /* Time O(N * M) | Space O(N * M) */
+
+  search(text1, text2, tabu); /* Time O(N * M) | Space O(N * M) */
+
+  return tabu[0][0];
+};
+
+var initTabu = (text1, text2) =>
+  new Array(text1.length + 1)
+    .fill() /* Time O(N) | Space O(N) */
+    .map(() =>
+      new Array(text2.length + 1).fill(0)
+    ); /* Time O(M) | Space O(M) */
+
+var search = (text1, text2, tabu) => {
+  const [n, m] = [text1.length, text2.length];
+
+  for (let x = n - 1; 0 <= x; x--) {
+    /* Time O(N) */
+    for (let y = m - 1; 0 <= y; y--) {
+      /* Time O(M) */
+      tabu[x][y] =
+        text1[x] === text2[y] /* Space O(N * M) */
+          ? tabu[x + 1][y + 1] + 1
+          : Math.max(tabu[x + 1][y], tabu[x][y + 1]);
     }
   }
-
-  console.log(dp);
-  return dp[m][n];
+  console.log(tabu);
 };
+
 console.log(longestCommonSubsequence('abcde', 'ace')); // expect 3
 // console.log(longestCommonSubsequence('abc', 'def')); // expect 0
 // console.log(longestCommonSubsequence('abc', 'abc')); // expect 3
