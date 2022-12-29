b113b091bb7c2868de60b1b6990b9963f3f41d19
Mark Charles Smith <markcharlessmith@gmail.com>
2022-12-29 11:05:44 -0800

    removed unneeded code from top-k-frequent-elements.js

--- top-k-frequent-elements.js
+++ top-k-frequent-elements.js
@@ -19,40 +19,6 @@ It is guaranteed that the answer is unique.
  * @param {number} k
  * @return {number[]}
  */
-// var topKFrequent = function (nums, k) {
-//   // create an object
-//   let obj = {};
-//   // const result = [];
-//   // sort the array
-//   nums = nums.sort((a, b) => a - b);
-//   // iterate through nums
-//   for (let i = 0; i < nums.length; i++) {
-//     if (!obj[nums[i]]) obj[nums[i]] = 1;
-//     else obj[nums[i]] += 1;
-//   }
-//   console.log(obj);
-//   // create multiple objects;
-//   // console.log(Object.entries(obj));
-//   // // sort the entries by the frequency of the number occurring
-//   // const entries = Object.entries(obj).sort((a, b) => b[1] - a[1]);
-//   // console.log(entries);
-//   const result = [];
-
-//   // for (let i = 0; i < k; i++) {
-//   //   while (k > 0) {
-//   //     result.push(entries[0][0]);
-//   //     // entries[0].shift();
-//   //     k--;
-//   //   }
-//   // }
-
-//   console.log(entries);
-//   console.log(result);
-//   // for (let i = 0; i < result.length; i++) {
-//   //   result[i].splice(0, i);
-//   // }
-//   return result;
-// };
 
 var topKFrequent = function (nums, k) {
   const result = [];
