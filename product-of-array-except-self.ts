725336753f3c89cdda4418840a8a71bcc8270998
Mark Charles Smith <markcharlessmith@gmail.com>
2023-01-02 20:21:27 -0800

    added solution for product-of-array-except-self.ys

--- product-of-array-except-self.ts
+++ product-of-array-except-self.ts
@@ -21,23 +21,42 @@ The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit int
  * @param {number[]} nums
  * @return {number[]}
  */
+// var productExceptSelf = function(nums: number[]) {
+//   const answer: number[] = [];
+//   // answer[i] equals the product of all elements of nums except nums[i]
+//   // .reduce()
+
+//   let tempArr: number[] = [];
+
+//   for (let i = 0; i < nums.length; i++) {
+//     tempArr = nums;
+//     console.log(tempArr)
+//     let push = nums.reduce((acc, curr) => acc * curr)
+//     answer.push(push)
+//     console.log(answer)
+//     // tempArr = nums;
+//     nums.splice(i, 1);
+//   }
+//   return answer;
+// };
+
+// 
 var productExceptSelf = function(nums: number[]) {
-  const answer: number[] = [];
-  // answer[i] equals the product of all elements of nums except nums[i]
-  // .reduce()
-
-  let tempArr: number[] = [];
-
-  for (let i = 0; i < nums.length; i++) {
-    tempArr = nums;
-    console.log(tempArr)
-    tempArr.splice(i, 1);
-    answer.push(nums.reduce((acc, curr) => acc * curr))
-    console.log(answer)
-    // tempArr = nums;
+  const result: number[] = [];
+  let productSoFar: number = 1;
+  for (let i: number = 0; i < nums.length; i++) {
+      result[i] = productSoFar
+      productSoFar *= nums[i]
   }
-  return answer;
+  console.log(result)
+  productSoFar = 1
+  for (let j: number = nums.length-1; j >= 0; j--) {
+      result[j] *= productSoFar
+      productSoFar *= nums[j]
+  }
+  console.log(result)
+  return result;
 };
 
 console.log(productExceptSelf([1,2,3,4])) // expect [24,12,8,6]
-// console.log(productExceptSelf([-1,1,0,-3,3])) // expect [[0,0,9,0,0]]
+console.log(productExceptSelf([-1,1,0,-3,3])) // expect [[0,0,9,0,0]]
