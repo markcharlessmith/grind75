98b89730fd03bf7fac8ff761076aca2d22b15c5c
Mark Charles Smith <markcharlessmith@gmail.com>
2023-01-04 15:03:41 -0800

    converted solution for length-of-last-word to TypeScript

--- length-of-last-word.ts
+++ length-of-last-word.ts
@@ -21,11 +21,11 @@ Explanation: The last word is "joyboy" with length 6.*/
  * @param {string} s
  * @return {number}
  */
-var lengthOfLastWord = function (s) {
+var lengthOfLastWord = function (s: string) {
   // define a word
   // a word is a substr beginning and ending with a non-space character
   // iterate backwards through the string
-  let substr = 0;
+  let substr: number = 0;
   for (let i = s.length - 1; i >= 0; i--) {
     while (s[i] === ' ') {
       i--;
